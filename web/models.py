074bd0174350b994aa4cc754dbfb3dc639c9660e
kian edjtehadi <ejtehadi.rig@gmail.com>
2021-04-19 18:22:03 +0430

    post model is there

--- web/models.py
+++ web/models.py
@@ -1,3 +1,10 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+class post(models.Model):
+    title = models.CharField(max_length=200)
+    text = models.CharField(max_length=355)
+    date = models.DateTimeField('publish date')
+    price = models.BigIntegerField('USD')
+    user = models.ForeignKey(User, models.CASCADE)
