2d600f7673ab2b5fa683767e1f694cc3a2058b83
kian edjtehadi <ejtehadi.rig@gmail.com>
2021-04-19 19:31:52 +0430

    purchase request post and some change in admin view

--- web/models.py
+++ web/models.py
@@ -8,3 +8,14 @@ class post(models.Model):
     date = models.DateTimeField('publish date')
     price = models.BigIntegerField('USD')
     user = models.ForeignKey(User, models.CASCADE)
+    def __str__(self):
+        return "%s - %i USD -%s" %(self.title, self.price, self.date)
+
+class Purchaseinq(models.Model):
+    title = models.CharField(max_length=200)
+    text = models.CharField(max_length=355)
+    date = models.DateTimeField('publish date')
+    priceTarget = models.BigIntegerField('USD')
+    user = models.ForeignKey(User, models.CASCADE)
+    def __str__(self):
+        return "%s - %i Target Price (USD) - %s" %(self.title, self.priceTarget, self.date)
